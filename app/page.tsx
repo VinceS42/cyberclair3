0559dc995c68626ae11f12193585b51fbd0b76c5
VinceS42 <v.fayon.pro@gmail.com>
2024-01-10 14:14:31 +0100

    [Cyber1] feature : fixe icon et police

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
     title: "CyberClair3",
     description: "",
-    icons: {
-        icon: "/Nico.ico",     
-    },
+
 };
 
 export default function Home() {
