b6c82d01c79c487fa38c5dcd3fac18477d154eb9
Julien Tavernier <julien.tavernier.pro@gmail.com>
2024-01-10 15:03:23 +0100

    [Cyber1] FIX: ajout d'un hover sur les btns

--- app/components/dashboard/Sidebar/CardAccount/index.tsx
+++ app/components/dashboard/Sidebar/CardAccount/index.tsx
@@ -24,34 +24,33 @@ export default function CardAccount({
     return (
         <div className="mb-3 shadow-xl">
             <div className="p-2.5 bg-[#202324] rounded-xl">
-                <div className="text-white flex items-center px-2.5 py-2.5 pb-4.5">
+                <div className="flex items-center gap-x-4 text-white px-2.5 py-2.5 pb-4.5">
                     <div className="relative w-10 h-10">
                         <Image
                             src={avatar}
-                            alt="Nicol le beaugoss"
+                            alt="Nico le beaugoss"
                             className="rounded-full object-cover"
-                            layout="fill"
+                            fill
                         />
                     </div>
-                    <div className="ml-4 mr-4">
+                    <div>
                         <div className="flex flex-row gap-x-1 font-semibold text-sm">
                             <span>{firstName}</span>
                             <span>{lastName}</span>
                         </div>
-                        <div></div>
                         <div className="font-medium text-xs text-[#e8ecef]/[.5]">
                             <span>{email}</span>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-row">
-                    <button className="flex gap-x-2 items-center w-full h-12 font-semibold transition-colors hover:text-[#0084ff] text-sm">
-                        <ArrowRightStartOnRectangleIcon className="h-5 w-5 opacity-30" />
-                        <span>Log out</span>
+                    <button className="flex gap-x-2 items-center w-full h-12 font-semibold text-sm group">
+                        <ArrowRightStartOnRectangleIcon className="h-5 w-5 opacity-30 group-hover:opacity-100 transition duration-100 ease-in-out" />
+                        <span className="text-white opacity-50 group-hover:opacity-100 transition duration-100 ease-in-out">Log out</span>
                     </button>
-                    <button className="flex gap-x-2 items-center w-full h-12 font-semibold transition-colors hover:text-[#0084ff] text-sm">
-                        <Cog6ToothIcon className="h-5 w-5 opacity-30" />
-                        <span>Settings</span>
+                    <button className="flex gap-x-2 items-center w-full h-12 font-semibold text-sm group">
+                        <Cog6ToothIcon className="h-5 w-5 opacity-30 group-hover:opacity-100 transition duration-100 ease-in-out" />
+                        <span className="text-white opacity-50 group-hover:opacity-100 transition duration-100 ease-in-out">Settings</span>
                     </button>
                 </div>
                 <BtnUpgrade isPremium={isPremium} />
